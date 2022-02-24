4dbc64be8f20187b387930c05678f341899cbcd5
John <G00367771@gmit.ie>
2022-02-24 18:42:43 +0000

    Added event location markers

--- even_ticket/lib/widgets/home_widgets/maps_widget.dart
+++ even_ticket/lib/widgets/home_widgets/maps_widget.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class MapScreen extends StatefulWidget {
   @override
@@ -8,12 +10,54 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  Location currentLocation = Location();
+  Set<Marker> _markers = {};
   static const _initialCameraPosition = CameraPosition(
     target: LatLng(53.3608176244587, -6.251144528771498),
-    zoom: 11.5,
+    zoom: 7,
   );
 
+  // void getLocation() async {
+  //   var location = await currentLocation.getLocation();
+  //   currentLocation.onLocationChanged.listen((LocationData loc) {
+  //     _googleMapController
+  //         ?.animateCamera(CameraUpdate.newCameraPosition(new CameraPosition(
+  //       target: LatLng(loc.latitude ?? 0.0, loc.longitude ?? 0.0),
+  //       zoom: 8.0,
+  //     )));
+  //     print(loc.latitude);
+  //     print(loc.longitude);
+  //     setState(() {
+  //       _markers.add(Marker(
+  //           markerId: MarkerId('Home'),
+  //           position: LatLng(loc.latitude ?? 0.0, loc.longitude ?? 0.0)));
+  //     });
+  //   });
+  // }
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   setState(() {
+  //     getLocation();
+  //   });
+  // }
+
   late GoogleMapController _googleMapController;
+
+  // Marker _CurrentPosition = Marker(
+  //   markerId: MarkerId('CurrentPosition'),
+  //   infoWindow: InfoWindow(title: 'Current location'),
+  //   icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
+  //   position: LatLng(),
+  // );
+
+  static final Marker _CrokeParkMarker = Marker(
+    markerId: MarkerId('CrokePark'),
+    infoWindow: InfoWindow(title: 'Croke Park'),
+    icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
+    position: LatLng(53.3608176244587, -6.251144528771498),
+  );
 
   @override
   void dispose() {
@@ -25,8 +69,10 @@ class _MapScreenState extends State<MapScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
-        myLocationButtonEnabled: false,
+        myLocationButtonEnabled: true,
+        myLocationEnabled: true,
         zoomControlsEnabled: false,
+        markers: {_CrokeParkMarker},
         initialCameraPosition: _initialCameraPosition,
         onMapCreated: (controller) => _googleMapController = controller,
       ),
