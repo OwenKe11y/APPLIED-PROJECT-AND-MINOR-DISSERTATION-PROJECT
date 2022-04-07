fc69899231fe8f221b9426c02d0e8699b93a0531
EndaGoggin <48314652+EndaGoggin@users.noreply.github.com>
2022-04-07 13:22:32 +0100

    Added flutter method for delete

--- even_ticket/lib/services/http_methods.dart
+++ even_ticket/lib/services/http_methods.dart
@@ -328,6 +328,33 @@ Future<void> getEvents() async {
   }
 }
 
+// Delete Event
+Future<String> deleteEvent(String event_name) async {
+  final response = await http.delete(
+    Uri.parse('http://eventicketapi.herokuapp.com/api/events'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+      "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+      "Access-Control-Allow-Credentials":
+          "true", // Required for cookies, authorization headers with HTTPS
+      "Access-Control-Allow-Headers":
+          "Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,locale",
+      "Access-Control-Allow-Methods": "GET, OPTIONS"
+    },
+    body: jsonEncode({
+      'title': event_name,
+    }),
+  );
+
+  if (response.statusCode == 200) {
+    // If the server did return a 201 CREATED response,
+    // then parse the JSON.
+    return "OK";
+  } else {
+    throw Exception("Failed to delete event");
+  }
+}
+
 Future<String> createTickets(String eventName, String owner, int amount) async {
   final response = await http.post(
     Uri.parse('http://eventicketapi.herokuapp.com/api/tickets/create'),
